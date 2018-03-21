6d74cc05bc927cd28f4a5ab6f2791e36a7c20305
Jose Sierra <jsierrav14@gmail.com>
2018-03-21 00:55:38 -0500

    App base

---
+++ app/src/main/java/com/app/ing/appsells/data/User.java
@@ -0,0 +1,15 @@
+package com.app.ing.appsells.data;
+
+/**
+ * Created by jose on 20/03/18.
+ */
+
+public class User {
+
+    private String name;
+    private String[] typesProduct;
+    private double latitud;
+    private double longitud;
+
+
+}
